444d2a2d08f63585c8d653b6631cc9c178150c58
wyh196646 <1966463192@qq.com>
2022-01-08 23:25:22 +0800

    "bupt"

--- data_processing/Utils.py
+++ data_processing/Utils.py
@@ -16,6 +16,12 @@ import functools
 from functools import partial
 import pydicom
 import json
+import scipy
+from skimage import measure, morphology
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+
+
+
 
 def detect_dcm_or_nii(patient_path):
     dir=Path(patient_path).parent[0]
@@ -84,13 +90,107 @@ def get_file(root_path,all_files=[]):
 #         img = sitk.GetArrayFromImage(itk_img)
 
     
-    data = json.dumps(data_info)
-    f2 = open('new_json.json', 'w')
-    f2.write(data)
-    f2.close()                
+#     data = json.dumps(data_info)
+#     f2 = open('new_json.json', 'w')
+#     f2.write(data)
+#     f2.close()                
+
+def resample(image, scan, new_spacing=[1,1,1]):
+    # Determine current pixel spacing
+    spacing = np.array([scan[0].SliceThickness] + scan[0].PixelSpacing, dtype=np.float32)
+
+    resize_factor = spacing / new_spacing
+    new_real_shape = image.shape * resize_factor
+    new_shape = np.round(new_real_shape)
+    real_resize_factor = new_shape / image.shape
+    new_spacing = spacing / real_resize_factor
+    
+    image = scipy.ndimage.interpolation.zoom(image, real_resize_factor, mode='nearest')
+    
+    return image, new_spacing
+
+def plot_3d(image, threshold=-300):
+    
+    # Position the scan upright, 
+    # so the head of the patient would be at the top facing the camera
+    p = image.transpose(2,1,0)
+    
+    verts, faces = measure.marching_cubes(p, threshold)
+
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Fancy indexing: `verts[faces]` to generate a collection of triangles
+    mesh = Poly3DCollection(verts[faces], alpha=0.70)
+    face_color = [0.45, 0.45, 0.75]
+    mesh.set_facecolor(face_color)
+    ax.add_collection3d(mesh)
+
+    ax.set_xlim(0, p.shape[0])
+    ax.set_ylim(0, p.shape[1])
+    ax.set_zlim(0, p.shape[2])
+
+    plt.show()
 
 
-def load_scan(filelist):
+def largest_label_volume(im, bg=-1):
+    vals, counts = np.unique(im, return_counts=True)
+
+    counts = counts[vals != bg]
+    vals = vals[vals != bg]
+
+    if len(counts) > 0:
+        return vals[np.argmax(counts)]
+    else:
+        return None
+
+def segment_lung_mask(image, fill_lung_structures=True):
+    
+    # not actually binary, but 1 and 2. 
+    # 0 is treated as background, which we do not want
+    binary_image = np.array(image > -320, dtype=np.int8)+1
+    labels = measure.label(binary_image)
+    
+    # Pick the pixel in the very corner to determine which label is air.
+    #   Improvement: Pick multiple background labels from around the patient
+    #   More resistant to "trays" on which the patient lays cutting the air 
+    #   around the person in half
+    background_label = labels[0,0,0]
+    
+    #Fill the air around the person
+    binary_image[background_label == labels] = 2
+    
+    
+    # Method of filling the lung structures (that is superior to something like 
+    # morphological closing)
+    if fill_lung_structures:
+        # For every slice we determine the largest solid structure
+        for i, axial_slice in enumerate(binary_image):
+            axial_slice = axial_slice - 1
+            labeling = measure.label(axial_slice)
+            l_max = largest_label_volume(labeling, bg=0)
+            
+            if l_max is not None: #This slice contains some lung
+                binary_image[i][labeling != l_max] = 1
+
+    
+    binary_image -= 1 #Make the image actual binary
+    binary_image = 1-binary_image # Invert it, lungs are now 1
+    
+    # Remove other air pockets insided body
+    labels = measure.label(binary_image, background=0)
+    l_max = largest_label_volume(labels, bg=0)
+    if l_max is not None: # There are air pockets
+        binary_image[labels != l_max] = 0
+ 
+    return binary_image
+
+def load_scan(filelist):#
+    '''
+    我们有可能根本不需要这一步
+    就可以得到垂直方向的间距，但其实这么用也不是不可以，尽量适配公开数据集
+    的接口就可以
+    '''
     #slices = [dicom.read_file(path + '/' + s) for s in os.listdir(path)]
     slices = [pydicom.dcmread(s) for s in filelist]
     slices.sort(key = lambda x: float(x.ImagePositionPatient[2]))
@@ -104,6 +204,13 @@ def load_scan(filelist):
         
     return slices
 
+
+def chunks(l, n):
+    # Credit: Ned Batchelder
+    # Link: http://stackoverflow.com/questions/312443/how-do-you-split-a-list-into-evenly-sized-chunks
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
 
 def search_label_(path):
     valid_label=[]
